d891db0198d9a1d3fb080dba81e333bf9c12e52e
Vasilis Nicolaou <vaslabsco@gmail.com>
2020-04-30 21:53:03 +0100

    draw the architecture

--- build.sbt
+++ build.sbt
@@ -3,3 +3,29 @@ name := "balcony"
 version := "0.1"
 
 scalaVersion := "2.13.2"
+
+
+lazy val balcony = (project in file("."))
+  .settings(commonSettings)
+  .aggregate(database, protocol, model)
+
+lazy val `balcony-cli` = (project in file("balcony-cli"))
+  .settings(commonSettings)
+  .dependsOn(database)
+
+lazy val database = (project in file("database"))
+  .settings(libraryDependencies ++= Dependencies.Module.database)
+  .settings(commonSettings)
+  .dependsOn(protocol)
+
+lazy val protocol = (project in file("protocol"))
+  .settings(libraryDependencies ++= Dependencies.Module.protocol)
+  .settings(commonSettings)
+  .dependsOn(model)
+
+lazy val model = (project in file("model"))
+  .settings(commonSettings)
+
+lazy val commonSettings = Seq(
+  scalaVersion in ThisBuild := "2.13.2"
+)
---
+++ project/Dependencies.scala
@@ -0,0 +1,31 @@
+import sbt._
+
+object Dependencies {
+
+  object Version {
+    val jgit = "v5.7.0.202003110725-r"
+    val scalatest = "3.1.1"
+    val scalacheck = "1.14.1"
+    object cats {
+      val core = "2.1.1"
+      val effect = "2.1.3"
+    }
+  }
+
+  object Library {
+    val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % Version.jgit
+    val scalatest = "org.scalactic" %% "scalactic" % Version.scalatest % Test
+    val scalacheck = "org.scalacheck" %% "scalacheck" % Version.scalacheck % Test
+    object cats {
+      val core = "org.typelevel" %% "cats-core" % Version.cats.core
+      val laws = "org.typelevel" %% "cats-effect-laws" % "2.1.3" % Test
+      val effect = "org.typelevel" %% "cats-effect" % Version.cats.effect
+    }
+  }
+
+  object Module {
+    import Library._
+    val protocol = Seq(cats.core, cats.laws, scalatest, scalacheck)
+    val database = Seq(jgit, scalatest, scalacheck, cats.effect)
+  }
+}
